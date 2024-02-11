a769a07a335b26443d45f7d6f2906658f212d5f3
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2024-02-11 12:13:12 +0400

    feat: Add OrderPlacedEvent and Kafka integration

---
+++ src/main/java/dev/abhiroopsantra/orderservice/event/OrderPlacedEvent.java
@@ -0,0 +1,10 @@
+package dev.abhiroopsantra.orderservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor public class OrderPlacedEvent {
+    private String orderNumber;
+}
--- src/main/java/dev/abhiroopsantra/orderservice/service/OrderServiceImpl.java
+++ src/main/java/dev/abhiroopsantra/orderservice/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package dev.abhiroopsantra.orderservice.service;
 
 import dev.abhiroopsantra.orderservice.dto.*;
+import dev.abhiroopsantra.orderservice.event.OrderPlacedEvent;
 import dev.abhiroopsantra.orderservice.exception.BadRequestException;
 import dev.abhiroopsantra.orderservice.model.Order;
 import dev.abhiroopsantra.orderservice.model.OrderLineItem;
@@ -8,28 +9,26 @@ import dev.abhiroopsantra.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.*;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class OrderServiceImpl implements OrderService {
-    private final ModelMapper modelMapper;
-    private final OrderRepository orderRepository;
-    private final WebClient.Builder webClientBuilder;
-    private final ObjectMapper objectMapper;
+@Service @RequiredArgsConstructor @Slf4j public class OrderServiceImpl implements OrderService {
+    public final  KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+    private final ModelMapper                             modelMapper;
+    private final OrderRepository                         orderRepository;
+    private final WebClient.Builder                       webClientBuilder;
+    private final ObjectMapper                            objectMapper;
 
-    @Override
-    public ApiResponse createOrder(OrderRequest orderRequest) {
+    @Override public ApiResponse createOrder(OrderRequest orderRequest) {
         Order order = modelMapper.map(orderRequest, Order.class);
 
         List<OrderLineItem> orderLineItems = orderRequest.getOrderLineItems().stream()
-                .map(orderLineItemRequest -> mapToOrderLineItem(
-                        orderLineItemRequest, order)).toList();
+                                                         .map(orderLineItemRequest -> mapToOrderLineItem(
+                                                                 orderLineItemRequest, order)).toList();
 
         order.setOrderLineItems(orderLineItems);
 
@@ -39,9 +38,9 @@ public class OrderServiceImpl implements OrderService {
                 orderRequest.getOrderLineItems().stream().map(this::mapToAvailabilityItemsRequest).toList());
 
         ApiResponse response = webClientBuilder.build().post()
-                .uri("http://inventory-service/api/v1/inventory/checkItemsAvailability")
-                .bodyValue(checkOrderAvailabilityRequestDto).retrieve()
-                .bodyToMono(ApiResponse.class).block();
+                                               .uri("http://inventory-service/api/v1/inventory/checkItemsAvailability")
+                                               .bodyValue(checkOrderAvailabilityRequestDto).retrieve()
+                                               .bodyToMono(ApiResponse.class).block();
 
         if (response == null) {
             throw new BadRequestException("Product is not in stock or Inventory Service is not reachable.");
@@ -54,19 +53,20 @@ public class OrderServiceImpl implements OrderService {
         }
 
         List<InventoryResponse> inventoryResponseList = dataList.stream().filter(item -> item instanceof LinkedHashMap)
-                .map(item -> objectMapper.convertValue(item,
-                        InventoryResponse.class
-                )).toList();
+                                                                .map(item -> objectMapper.convertValue(item,
+                                                                                                       InventoryResponse.class
+                                                                                                      )).toList();
         boolean allProductsInStock = inventoryResponseList.stream().allMatch(InventoryResponse::isInStock);
 
         if (!allProductsInStock) {
             throw new BadRequestException("Product is not in stock");
         }
         orderRepository.save(order);
+        kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getId().toString()));
         log.info("Order created: {}", order.getId());
 
         ApiResponse apiResponse = new ApiResponse();
-        apiResponse.errCode = "201";
+        apiResponse.errCode    = "201";
         apiResponse.errMessage = "Order created successfully";
 
         return apiResponse;
